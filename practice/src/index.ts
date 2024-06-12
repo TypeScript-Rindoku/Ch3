30725f01f753d4433a1b6be4db4d141cf0cb430c
junohm410 <junohm410@gmail.com>
2024-06-12 15:44:59 +0900

    いろいろなプロパティ名の指定方法を試す

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,7 +1,24 @@
+// const obj = {
+//   "foo": 123,
+//   "foo bar": -500,
+//   '↑↓↑': "",
+// };
+
+// const name = input ? input : "名無し",
+
+// const user = {
+//   name,
+//   age: 20,
+// };
+
+// const obj = {
+//   1: "one",
+//   2.05: "two point o five",
+// };
+
+const propName = "foo";
 const obj = {
-  foo: 123,
-  bar: "Hello, world!"
+  [propName]: 123,
 };
 
 console.log(obj.foo);
-console.log(obj.bar);
