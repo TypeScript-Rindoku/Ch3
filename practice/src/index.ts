5683426c6e0083b5521f7ef6da817bae277f6ac7
moegi29 <sedmikrasky0209@gmail.com>
2024-06-26 15:45:22 +0900

    型チェック,type文,interface宣言

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,7 +1,39 @@
-const obj = {
-  foo: 123,
-  bar: "Hello, world!"
-};
+// const obj = {
+//   foo: 123,
+//   bar: "Hello, world!"
+// };
 
-console.log(obj.foo);
-console.log(obj.bar);
+// console.log(obj.foo);
+// console.log(obj.bar);
+
+// const name = input ? input : "名無し";
+// const user = {
+// name: name,
+// age: 20,
+// };
+
+// type FooBarObj = {
+//   foo: number;
+//   bar: string;
+// };
+// const obj: FooBarObj = {
+//     foo: 123,
+//     bar: "Hello, world!"
+// };
+
+// type UserId = string;
+// const id: UserId = "uhyo";
+
+// type FooObj = { foo: number };
+// type Myobj = FooObj;
+
+// const obj: Myobj = { foo: 0};
+
+interface FooBarObj {
+  foo: number;
+  bar: string;
+}
+const obj: FooBarObj = {
+    foo: 0,
+    bar: "string"
+};
